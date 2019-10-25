0efe068d07fb8897e4c7233a1bdc79af17dbc46a
lzkmylz <lzkmylz@163.com>
2019-10-25 23:31:03 +0800

    add buildspec and remove build from .gitignore for CI/CD

---
+++ build/precache-manifest.150c4938fa73cb321f8beb3f7c5b2069.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2efab2b1e4c0537d08bcda775988feb7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d9c6b1e5ecea52caad74",
+    "url": "/static/css/2.399fa8cc.chunk.css"
+  },
+  {
+    "revision": "39552f8f01fe23831eff",
+    "url": "/static/css/main.64c2ec77.chunk.css"
+  },
+  {
+    "revision": "d9c6b1e5ecea52caad74",
+    "url": "/static/js/2.9fe12c44.chunk.js"
+  },
+  {
+    "revision": "39552f8f01fe23831eff",
+    "url": "/static/js/main.2a75d668.chunk.js"
+  },
+  {
+    "revision": "ed9c2b8b8e9bcd673fe0",
+    "url": "/static/js/runtime-main.1cd85038.js"
+  }
+]);
