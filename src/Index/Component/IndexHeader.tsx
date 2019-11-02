0b3971940a94ece42e3037f21a75b610138f87ff
lzkmylz <lzkmylz@163.com>
2019-11-02 19:36:34 +0800

    fix mobil style of header login part

--- src/Index/Component/IndexHeader.tsx
+++ src/Index/Component/IndexHeader.tsx
@@ -28,6 +28,12 @@ class IndexHeader extends React.Component {
     const menu = (
       <AntdMenu>
         <AntdMenu.Item>
+          <a>Dashboard</a>
+        </AntdMenu.Item>
+        <AntdMenu.Item>
+          <a>Reset Password</a>
+        </AntdMenu.Item>
+        <AntdMenu.Item>
           <div onClick={this.handleLogOut} >Log Out</div>
         </AntdMenu.Item>
       </AntdMenu>
@@ -41,27 +47,55 @@ class IndexHeader extends React.Component {
           <IconButton onClick={this.handleClick} >
             <List htmlColor="#ffffff" fontSize="large" />
           </IconButton>
-          <Menu
-            id="simple-menu"
-            anchorEl={this.state.anchorEl}
-            keepMounted
-            open={Boolean(this.state.anchorEl)}
-            onClose={this.handleClose}
-          >
-            <MenuItem onClick={this.handleClose}>Products</MenuItem>
-            <MenuItem onClick={this.handleClose}>Pricing</MenuItem>
-            <MenuItem onClick={this.handleClose}>Support</MenuItem>
-            <MenuItem onClick={this.handleClose}>
-              <a className="header-menuitem" href="/login" >Sign In</a>
-            </MenuItem>
-          </Menu>
+          {
+            Boolean(UserStore.accessToken) ? (
+              <Menu
+                id="simple-menu"
+                anchorEl={this.state.anchorEl}
+                keepMounted
+                open={Boolean(this.state.anchorEl)}
+                onClose={this.handleClose}
+              >
+                <MenuItem onClick={this.handleClose}>Products</MenuItem>
+                <MenuItem onClick={this.handleClose}>Pricing</MenuItem>
+                <MenuItem onClick={this.handleClose}>Support</MenuItem>
+                <MenuItem onClick={this.handleClose}>
+                  <a className="header-menuitem" href="/" >Dashboard</a>
+                </MenuItem>
+                <MenuItem onClick={this.handleClose}>
+                  <a className="header-menuitem" href="/" >Reset Password</a>
+                </MenuItem>
+                <MenuItem onClick={this.handleClose}>
+                  <div className="header-menuitem" onClick={this.handleLogOut} >Log Out</div>
+                </MenuItem>
+              </Menu>
+            ) : (
+              <Menu
+                id="simple-menu"
+                anchorEl={this.state.anchorEl}
+                keepMounted
+                open={Boolean(this.state.anchorEl)}
+                onClose={this.handleClose}
+              >
+                <MenuItem onClick={this.handleClose}>Products</MenuItem>
+                <MenuItem onClick={this.handleClose}>Pricing</MenuItem>
+                <MenuItem onClick={this.handleClose}>Support</MenuItem>
+                <MenuItem onClick={this.handleClose}>
+                  <a className="header-menuitem" href="/login" >Sign In</a>
+                </MenuItem>
+                <MenuItem onClick={this.handleClose}>
+                  <a className="header-menuitem" href="/register" >Sign Up</a>
+                </MenuItem>
+              </Menu>
+            )
+          }
         </div>
         <ul className="header-nav-container desktop" >
           <li className="header-nav-item" ><p>Products</p></li>
           <li className="header-nav-item" ><p>Pricing</p></li>
           <li className="header-nav-item" ><p>Support</p></li>
         </ul>
-        <div className="header-login-container" >
+        <div className="header-login-container desktop" >
           {
             Boolean(UserStore.accessToken) ? (
               <div>
@@ -81,7 +115,6 @@ class IndexHeader extends React.Component {
               </div>
             )
           }
-          
         </div>
       </div>
     );
