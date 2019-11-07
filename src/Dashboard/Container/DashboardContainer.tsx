019ff63044d18844a712874774250140bb123596
lzkmylz <lzkmylz@163.com>
2019-11-07 21:20:21 +0800

    basic add dashboard layout and style

--- src/Dashboard/Container/DashboardContainer.tsx
+++ src/Dashboard/Container/DashboardContainer.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Layout, Menu } from 'antd';
 import Header from '../../Index/Component/IndexHeader';
 import Footer from '../../Index/Component/IndexFooter';
 import '../Style/DashboardContainer.scss';
@@ -9,13 +10,33 @@ interface Iprops {
   }
 }
 
+const { Sider, Content } = Layout;
+
 class DashboardContainer extends React.Component<Iprops> {
   render() {
     return(
       <div className="dashboard-container" >
         <Header history={this.props.history} />
         <div className="dashboard-main" >
-
+          <Layout className="dashboard-layout" >
+            <Sider width={200} theme="dark" >
+              <Menu
+                mode="inline"
+                defaultSelectedKeys={['profile']}
+                style={{ height: '100%' }}
+              >
+                <Menu.Item key="profile" >
+                  Profile
+                </Menu.Item>
+                <Menu.Item key="analytics" >
+                  Analytics
+                </Menu.Item>
+              </Menu>
+            </Sider>
+            <Content>
+              Content
+            </Content>
+          </Layout>
         </div>
         <Footer />
       </div>
