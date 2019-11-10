a5a37a14304a367ee9287f75dfca241ba06fdfd0
lzkmylz <lzkmylz@163.com>
2019-11-10 20:49:45 +0800

    basic finish donate page waiting replace images

--- src/Index/Container/DonateContainer.tsx
+++ src/Index/Container/DonateContainer.tsx
@@ -1,7 +1,12 @@
 import React from 'react';
+import $ from 'jquery';
+import Clipboard from 'clipboard';
 import Header from '../Component/IndexHeader';
 import Footer from '../Component/IndexFooter';
 import '../Style/DonateContainer.scss';
+import AliPayQR from '../images/AliPayQR.png';
+import WeChanSQ from '../images/WeChanSQ.png';
+import BTCQR from '../images/BTCQR.png';
 
 interface Iprops {
   history: {
@@ -10,12 +15,65 @@ interface Iprops {
 }
 
 class DonateContainer extends React.Component<Iprops> {
+  
+  componentDidMount() {
+    var MainBox	=	$('#MainBox');
+    var QRBox	=	$('#QRBox');
+    var showQR = (QR: any) => {
+      var MainBox	=	$('#MainBox');
+      var QRBox	=	$('#QRBox');
+      if (QR) {
+        MainBox.css('background-image',`url(${QR})`);
+      }
+      $('#DonateText,#donateBox,#github').addClass('blur');
+      QRBox.fadeIn(300,function() {
+        MainBox.addClass('showQR');
+      });
+    }
+
+    $('#donateBox>li').click(function() {
+      var thisID	=	$(this).attr('id');
+      if (thisID === 'BTC') {
+        showQR(BTCQR);
+        new Clipboard('#BTCBn');
+      } else if (thisID === 'AliPay') {
+        showQR(AliPayQR);
+      } else if (thisID === 'WeChat') {
+        showQR(WeChanSQ);
+      }
+    });
+    MainBox.click(function(event) {
+      MainBox.removeClass('showQR').addClass('hideQR');
+      setTimeout (function(a) {
+        QRBox.fadeOut(300,function() {
+          MainBox.removeClass('hideQR');
+        });
+        $('#DonateText,#donateBox,#github').removeClass('blur');
+      },600);
+  
+    });
+  }
   render() {
     return (
       <div className="donate-container" >
         <Header history={this.props.history} />
         <div className="donate-main" >
-
+          <h1>This is a personal project</h1>
+          <h1>If you think this website can help you</h1>
+          <h1>Just donate for a cup of coffee</h1>
+          <div className="donate-plugin-container" >
+            <div id="DonateText" className="tr3">Sponsor</div>
+            <ul id="donateBox" className="list pos-f tr3">
+              <li id="PayPal"><a href="https://www.paypal.me/KaiyuanXie" target="_blank" rel="noopener noreferrer" >PayPal</a></li>
+              <li id="BTC" data-footnote="Copy addres and show QRCod"><button id="BTCBn"  data-clipboard-target="#btc-key">Bitcoin</button></li>
+              <li id="AliPay">AliPay</li>
+              <li id="WeChat">WeChat</li>
+            </ul>
+            <div id="QRBox" className="pos-f left-100">
+              <div id="MainBox"></div>
+            </div>
+	          <input id="btc-key" type="text" value="14JkMR68n4PBASB3TgvpjtaPTbfffSwFbW" readOnly ></input>
+          </div>
         </div>
         <Footer />
       </div>
