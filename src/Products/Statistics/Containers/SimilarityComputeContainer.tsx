765cc4964bc49a84608942d4aba6f8e706b7fbc1
lzkmylz <lzkmylz@163.com>
2019-11-23 17:54:35 +0800

    add similarity compute navbar and title

--- src/Products/Statistics/Containers/SimilarityComputeContainer.tsx
+++ src/Products/Statistics/Containers/SimilarityComputeContainer.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import { observer } from 'mobx-react';
+import { Breadcrumb } from 'antd';
 import Header from '../../../Index/Component/IndexHeader';
 import Footer from '../../../Index/Component/IndexFooter';
+import '../Style/SimilarityComputeContainer.scss';
 
 interface Iprops {
   history: {
@@ -17,7 +19,22 @@ class SimilarityComputeContainer extends React.Component<Iprops> {
       <div className="similarity-compute-container" >
         <Header history={this.props.history} />
         <div className="similarity-compute-main" >
-          Test
+          <div className="similarity-compute-navbar" >
+            <Breadcrumb>
+              <Breadcrumb.Item>
+                <a href="/">Home</a>
+              </Breadcrumb.Item>
+              <Breadcrumb.Item>
+                <a href="/products">Products</a>
+              </Breadcrumb.Item>
+            </Breadcrumb>
+          </div>
+          <div className="similarity-compute-title" >
+            <h1>
+              Stock Similarity Compute
+            </h1>
+          </div>
+          
         </div>
         <Footer />
       </div>
