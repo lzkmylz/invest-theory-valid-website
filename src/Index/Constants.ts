c7575e37f77de4359d394c20f5103e74616d820b
lzkmylz <lzkmylz@163.com>
2019-12-18 21:26:45 +0800

    waiting serverless function

--- src/Index/Constants.ts
+++ src/Index/Constants.ts
@@ -2,9 +2,6 @@ export var config = {
     cognito: {
         userPoolId: process.env.REACT_APP_AWS_INVESTVALID_USERPOOL_ID,
         userPoolClientId: process.env.REACT_APP_AWS_INVESTVALID_USERPOOL_CLIENT_ID
-    },
-    tushare: {
-        token: process.env.TUSHARE_TOKEN
     }
 };
 
--- src/Products/DL/Stores/DLStore.ts
+++ src/Products/DL/Stores/DLStore.ts
@@ -1,24 +1,13 @@
 import { observable, action } from 'mobx';
 import axios from 'axios';
 import moment from 'moment';
-import { config } from '../../../Index/Constants';
 
 class DLStore {
   @observable SHIndexData = [];
 
   @action getIndexData = () => {
     let url = "http://api.waditu.com";
-    let reqBody = {
-      api_name: "index_daily",
-      token: config.tushare.token,
-      params: {
-        ts_code: "399300.SZ",
-        start_date: moment().subtract(15, 'days').format('YYYYMMDD'),
-        end_date: moment().format("YYYYMMDD"),
-      },
-      fields: "",
-    };
-
+    
   }
 }
 
